60ad6a766856869844280c43e6ac1705670dbc1d
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-05-11 22:10:22 +0600

    add products

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 const AddItem = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/inventory';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
 
     return (
         <div className='w-50 mx-auto'>
 
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName")} />
-                <select {...register("gender")}>
-                    <option value="female">female</option>
-                    <option value="male">male</option>
-                    <option value="other">other</option>
-                </select>
-                <input type="submit" />
+                <input placeholder='Photo URL' className='mb-2' type="text" {...register("img")} />
+                <input placeholder='Product Name' className='mb-2' {...register("name", { required: true, maxLength: 20 })} />
+                <input placeholder='Supplier Name' className='mb-2' {...register("supplier", { required: true, maxLength: 20 })} />
+                <textarea placeholder='Description' className='mb-2' {...register("details")} />
+                <input className='btn btn-success' type="submit" value="Add New Product" />
             </form>
         </div>
     );
